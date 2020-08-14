b2a2b23fe369c6ce3054d511b3bc7d65a3681ad9
Fongdarren <66189539+Fongdarren@users.noreply.github.com>
2020-08-14 18:34:18 +0800

    last upload
    this is last project test a file

---
+++ Noname1.java
@@ -0,0 +1,10 @@
+class  Noname1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("好好在github上学习!");
+		System.out.println("i'm talking about a lifetime One year one month one day even on second's less makes it less than a lifetime .");
+		System.out.println("the chance of a lifetime");
+		
+	}
+}

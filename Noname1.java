a19ec0ed86886bbf08c88cce5eb1df9f62cea3e3
Fongdarren <66189539+Fongdarren@users.noreply.github.com>
2020-08-14 18:11:53 +0800

    Again Update!
    little update

--- Noname1.java
+++ Noname1.java
@@ -2,8 +2,9 @@ class  Noname1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello gitHub!");
+		System.out.println("Hello gitHub! ");
 		System.out.println("i'm talking about a lifetime One year one month one day even on second's less makes it less than a lifetime .");
 		System.out.println("the chance of a lifetime");
+		System.out.println("Hello gitHub! Again! ");
 	}
 }
